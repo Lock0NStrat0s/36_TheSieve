4782f2ec4998774bb8d381f5f9f3573eae56e72e
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-21 04:45:42 -0600

    initial commit

---
+++ 36_TheSieve/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _36_TheSieve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
